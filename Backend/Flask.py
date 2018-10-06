c000299f056badaf5818cd701db5b8125c7bc249
preet-kaur <gurpreet.saimy@gmail.com>
2018-10-06 12:43:01 +0530

    integrated backend with android, tested with dummy data

--- Backend/Flask.py
+++ Backend/Flask.py
@@ -1,24 +1,36 @@
+import os
 import random
 from werkzeug.routing import BaseConverter
 from flask import Flask, jsonify, request, json, session, render_template, redirect, url_for, Response
-import run_inference
+#import run_inference
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = os.path.basename("{0}/uploads".format(os.getcwd()))
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route('/upload')
 def upload_temp():
-   return render_template('upload.html')
+    abs_path = '/home/gurpreet/Documents/Visual-Aid/Backend/uploads'
+    if not os.path.exists(abs_path):
+        return os.abort(404)
+    files = os.listdir(abs_path)
+    return render_template('upload.html', files=files)
 
-@app.route('/uploader', methods = ['GET', 'POST'])
+
+@app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(f.filename)
-      message = run_inference.mess(f.filename)
-      print(message)
-      return Response(message)
+    if request.method == 'POST':
+        f = request.files['image']
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        # message = run_inference.mess(f.filename)     #image sent for testing
+        message = "Hello world!"  # dummy caption
+        print(message)
+        return message
+
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 if __name__ == "__main__":
-    app.run(host='192.168.43.171',port=8000)
+    app.run(host='0.0.0.0', port=8000)
